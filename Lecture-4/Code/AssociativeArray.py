517662bc16b5c03bc96d81ea6f3b887e2ed3d98c
Florian Eggenhofer <egg@informatik.uni-freiburg.de>
2016-11-22 13:39:00 +0100

    Fixed typos in Lecture 4 code

--- Lecture-4/Code/AssociativeArray.py
+++ Lecture-4/Code/AssociativeArray.py
@@ -1,13 +1,13 @@
 # creates a new map (called dictionary)
-countires = {"DE" : "Deutschland", \
+countries = {"DE" : "Deutschland", \
 	"EN" : "England"}
 
 # check if element exists
-if "EN" in countires:
-	print("Found %s!" % countires["EN"])
+if "EN" in countries:
+	print("Found %s!" % countries["EN"])
 
 # map key "DE" to value 0
-countires["DE"] = "Germany"
+countries["DE"] = "Germany"
 
 # delete key "DE"
-del countires["DE"]
+del countries["DE"]
