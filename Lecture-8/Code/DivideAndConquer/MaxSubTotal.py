6fb1c7d61e046a5e30c6709e476ee80209a9026d
Florian Eggenhofer <egg@tbi.univie.ac.at>
2016-12-19 01:00:46 +0100

    Added additonal changes to divide and conquer

--- Lecture-8/Code/DivideAndConquer/MaxSubTotal.py
+++ Lecture-8/Code/DivideAndConquer/MaxSubTotal.py
@@ -1,15 +1,11 @@
 def maxSubArray(X):
 	result = (X[0], 0, 0)
-	
 	# n loops -> O(n)
 	for i in range(0, len(X)):
 		subSum = 0
-		
 		# max n loops -> O(n)
 		for j in range(i, len(X)):
-			subSum += X[j] # O(1)
-			
+			subSum += X[j] # O(1)		
 			if result[0] < subSum: # O(1)
 				result = (subSum, i, j)
-	
 	return result
--- Lecture-8/Code/DivideAndConquer/MaxSubTotal_DivideAndConquer.py
+++ Lecture-8/Code/DivideAndConquer/MaxSubTotal_DivideAndConquer.py
@@ -1,14 +1,14 @@
 def maxSubArray(X, i, j):
-	if i == j: # trivial
-		return (X[i], i, i)
-	
+	if i == j: #trivial case
+		return (X[i], i, i)	
 	m = (i + j) / 2
+        #recursive Subsolutions for A,B
 	A = maxSubArray(X, i, m)
 	B = maxSubArray(X, m + 1, j)
-	
+	#rmax and lmax for bordercase C
 	C1 = rmax(X, i, m)
 	C2 = lmax(X, m + 1, j)
 	C = (C1[0] + C2[0], C1[1], C2[1])
-	
+	#Solution results from A,B,C
 	return max([A, B, C], \
 		key=lambda item: item[0])
