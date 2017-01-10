bc193b08157209d3782b075ce7f2ed489faad7e8
Florian Eggenhofer <egg@informatik.uni-freiburg.de>
2017-01-10 11:46:32 +0100

    Added onslide events for maxsub runtime

--- Lecture-9/Code/DivideAndConquer/MaxSubTotal_Runtime.py
+++ Lecture-9/Code/DivideAndConquer/MaxSubTotal_Runtime.py
@@ -1,14 +1,14 @@
-def maxSubArray(X, i, j):@\onslide<2->@
-	if i == j:							# O(1)
-		return (X[i], i, i)				# O(1)
-	@\onslide<3->@
-	m = (i + j) / 2						# O(1)
-	A = maxSubArray(X, i, m)			# T(n/2)
-	B = maxSubArray(X, m + 1, j)		# T(n/2)
-	@\onslide<4->@
-	C1 = rmax(X, i, m)					# O(n)
-	C2 = lmax(X, m + 1, j)				# O(n)
-	C = (C1[0] + C2[0], C1[1], C2[1])	# O(1)
-	@\onslide<5->@
-	return max([A, B, C], \				# O(1)
+def maxSubArray(X, i, j):
+	if i == j:							@\onslide<3->@# O(1)@\onslide<1->@
+		return (X[i], i, i)				@\onslide<4->@# O(1)@\onslide<1->@
+	
+	m = (i + j) / 2						@\onslide<5->@# O(1)@\onslide<1->@
+	A = maxSubArray(X, i, m)			@\onslide<6->@# T(n/2)@\onslide<1->@
+	B = maxSubArray(X, m + 1, j)		@\onslide<7->@# T(n/2)@\onslide<1->@
+
+	C1 = rmax(X, i, m)					@\onslide<8->@# O(n)@\onslide<1->@
+	C2 = lmax(X, m + 1, j)				@\onslide<9->@# O(n)@\onslide<1->@
+	C = (C1[0] + C2[0], C1[1], C2[1])	@\onslide<10->@# O(1)@\onslide<1->@
+
+	return max([A, B, C], \				@\onslide<11->@# O(1)@\onslide<1->@
 		key=lambda item: item[0])
