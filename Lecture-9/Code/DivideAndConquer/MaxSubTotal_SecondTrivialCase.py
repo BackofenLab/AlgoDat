2aaf8e580fbd396523b134b75531311d06d00d73
Tobias Faller <TobiasFaller@gmx.net>
2017-12-07 18:53:20 +0100

    Updated lecture-9

--- Lecture-9/Code/DivideAndConquer/MaxSubTotal_SecondTrivialCase.py
+++ Lecture-9/Code/DivideAndConquer/MaxSubTotal_SecondTrivialCase.py
@@ -3,11 +3,13 @@ def maxSubArray(X, i, j): @\onslide<2->@
 	# trivial: only one element
 	if i == j:
 		return (X[i], i, i) @\onslide<3->@
+
 	# trivial: only two elements
-	if i + 1 = j:
+	if i + 1 == j:
 		return max([
 			(X[i], i, i),
 			(X[j], j, j),
 			(X[i] + X[j], i, j)
 		], key=lambda item: item[0])
+
 	... # continue as before
