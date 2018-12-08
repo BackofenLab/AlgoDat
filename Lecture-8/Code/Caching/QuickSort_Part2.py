5b78f4856750784f279bd94dd5623ed7b0b7962b
Tobias Faller <TobiasFaller@gmx.net>
2018-12-08 14:59:23 +0100

    Fixed quicksort algorithm

--- Lecture-8/Code/Caching/QuickSort_Part2.py
+++ Lecture-8/Code/Caching/QuickSort_Part2.py
@@ -1,15 +1,15 @@
 def quicksort(l, start, end):
 	...
-	
+
 	while k > i:
 		while l[i] <= piv and i <= end and k > i:
 			i += 1
 		while l[k] > piv and k >= start and k >= i:
 			k -= 1
-		
+
 		if k > i: # swap elements
 			(l[i], l[k]) = (l[k], l[i])
-	
+
 	(l[start], l[k]) = (l[k], l[start])
 	quicksort(l, start, k - 1)
 	quicksort(l, k + 1, end)
